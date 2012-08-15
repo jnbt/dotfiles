04f1b99aed450acd5f2ffc461186ecf7f0b93405
Jonas Thiel <jt@neopoly.de>
2012-08-15 15:27:50 +0200

    shorthand for local projects

--- oh-my-zsh/custom/plugins/jnbt/jnbt.plugin.zsh
+++ oh-my-zsh/custom/plugins/jnbt/jnbt.plugin.zsh
@@ -1,2 +1,9 @@
 vhosts(){ mate /etc/apache2/extra/httpd-vhosts.conf; }
 hosts(){ mate /etc/hosts; }
+
+c(){ cd ~/Inwork/$1; }
+_c(){ _files -W ~/Inwork -/; }
+compdef _c c
+
+# autocorrect is more annoying than helpful
+unsetopt correct_all
