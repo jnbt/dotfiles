c8257e3eceda8a6857c8d99a67f672da98413189
Jonas Thiel <jt@neopoly.de>
2012-10-15 14:38:02 +0200

    Add plugin for bb

---
+++ oh-my-zsh/custom/plugins/bb/bb.plugin.zsh
@@ -0,0 +1,8 @@
+bbgit() {
+  git config user.name "Jonas Thiel"
+  git config user.email "jonasthiel+bb@googlemail.com"
+  echo -n "Name: "
+  git config user.name
+  echo -n "E-Mail: "
+  git config user.email
+}
