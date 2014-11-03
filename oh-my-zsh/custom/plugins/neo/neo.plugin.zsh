1ac949422f6a935523b81a579751746ef9120c17
Jonas Thiel <jt@neopoly.de>
2014-11-03 16:05:37 +0100

    Update neo files

--- oh-my-zsh/custom/plugins/neo/neo.plugin.zsh
+++ oh-my-zsh/custom/plugins/neo/neo.plugin.zsh
@@ -7,10 +7,30 @@ neogit() {
   git config user.email
 }
 
-neo(){ cd ~/Neopoly/$1; }
-_neo(){ _files -W ~/Neopoly -/; }
+neo(){ cd ~/devel/$1; }
+_neo(){ _files -W ~/devel -/; }
 compdef _neo neo
 
-resin(){ mate ~/Neopoly/resin/conf/$1; }
-_resin(){ _files -W ~/Neopoly/resin/conf/ -/; }
+resin(){ "${EDITOR:-vi}" ~/devel/resin/conf/$1; }
+_resin(){ _files -W ~/devel/resin/conf/ -/; }
 compdef _resin resin
+
+neoclone(){ cd ~/devel; git clone git@office.neopoly.de:$1; cd $1; gco develop; }
+
+PROJECT_HOST=doener
+PROJECT_HOST_FILE=~/.doener
+
+neoserver(){
+  if [[ -n $1 ]]; then
+    project=$1
+  else
+    project=${${PWD##*/}%%-*}
+  fi
+
+  if [[ -f $PROJECT_HOST_FILE ]]; then
+    sshpass -f $PROJECT_HOST_FILE ssh $project@$PROJECT_HOST
+  else
+    echo "(use sshpass via file: $PROJECT_HOST_FILE)"
+    ssh $project@$PROJECT_HOST $*
+  fi
+}
