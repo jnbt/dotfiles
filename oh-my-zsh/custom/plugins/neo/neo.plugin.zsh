8a4b20f477d76e97c32966b441904143096e1c2a
Jonas Thiel <jonas@thiel.io>
2015-05-27 14:12:30 +0200

    Add gitlab based neoclone command

--- oh-my-zsh/custom/plugins/neo/neo.plugin.zsh
+++ oh-my-zsh/custom/plugins/neo/neo.plugin.zsh
@@ -4,20 +4,30 @@ export SF_DIR="$HOME/devel/smartfox"
 # Aliases
 alias md5sum='md5 -r'
 
-neo(){ cd ~/devel/$1; }
-_neo(){ _files -W ~/devel -/; }
+neo()  { cd ~/devel/$1; }
+_neo() { _files -W ~/devel -/; }
 compdef _neo neo
 
-resin(){ "${EDITOR:-vi}" ~/devel/resin/conf/$1; }
-_resin(){ _files -W ~/devel/resin/conf/ -/; }
+resin()  { "${EDITOR:-vi}" ~/devel/resin/conf/$1; }
+_resin() { _files -W ~/devel/resin/conf/ -/; }
 compdef _resin resin
 
-neoclone(){ cd ~/devel; git clone git@office.neopoly.de:$1; cd $1; gco develop; }
+neoclone() {
+  git clone gitlab@gitlab.neopoly.de:$1;
+  cd $1;
+}
+
+neocloneold() {
+  cd ~/devel;
+  git clone git@office.neopoly.de:$1;
+  cd $1;
+  gco develop;
+}
 
 PROJECT_HOST=doener
 PROJECT_HOST_FILE=~/.doener
 
-neoserver(){
+neoserver() {
   if [[ -n $1 ]]; then
     project=$1
   else
