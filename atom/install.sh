86e89b993a6fe56fb2d69be79725f4ec2a860cd5
Jonas Thiel <jt@neopoly.de>
2015-07-06 10:40:39 +0200

    Add atom configuration

---
+++ atom/install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -e
+
+SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+_install_packages() {
+  apm install --packages-file "$SCRIPT_FOLDER/package-list.txt" --quiet
+}
+
+_install_config() {
+  was=$(pwd)
+  target="$HOME/.atom"
+  source="$SCRIPT_FOLDER"
+  if [ -d "$target" ]
+  then
+    echo "Config already present at \"$target\""
+  else
+    echo "Installing config \"$source\" to \"$target\""
+    ln -s "$source" "$target"
+  fi
+}
+
+install() {
+  _install_config
+  _install_packages
+}
+
+save() {
+  apm list --installed --bare > "$SCRIPT_FOLDER/package-list.txt"
+}
+
+case "$1" in
+  (save)
+    save
+    exit 1
+    ;;
+  (*)
+    install
+    exit 1
+    ;;
+esac
