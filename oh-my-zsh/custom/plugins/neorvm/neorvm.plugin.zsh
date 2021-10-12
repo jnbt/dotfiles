46e4951a97e875ea642c1fc06b077760094c670f
Jonas Thiel <jt@neopoly.de>
2021-10-12 22:31:50 +0200

    [zsh] Add rvm compatibility layer based on asdf

--- oh-my-zsh/custom/plugins/neorvm/neorvm.plugin.zsh
+++ oh-my-zsh/custom/plugins/neorvm/neorvm.plugin.zsh
@@ -12,4 +12,36 @@ then
     _directories -W `rvm gemdir`/gems;
   }
   compdef _chgem chgem
+else
+  rvm() {
+    case $1 in
+      use)
+        shift
+        full="$1"
+        version="${full%%@*}"
+        echo "Executing: asdf current ruby $version"
+        asdf shell ruby $version
+      ;;
+      install)
+        shift
+        full="$1"
+        version="${full%%@*}"
+        echo "Executing: asdf install ruby $version"
+        asdf install ruby $version
+      ;;
+      current)
+        asdf current ruby | head -n1 | awk '{print $1;}'
+      ;;
+      *)
+        echo "RVM compabality layer"
+        echo "USAGE: rvm [COMMAND] [OPTIONS]"
+        echo
+        echo "This is a compatibility layer based on asdf, only limited commands are supported:"
+        echo
+        echo "Commands:"
+        echo " install - Installs a specific Ruby version"
+        echo " current - Reports the currently used Ruby version"
+        echo " use     - Activate a specific Ruby version"
+    esac
+  }
 fi
